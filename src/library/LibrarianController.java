14b350dbe60a6a7fc6551ad4462939dc5b0c17df
Jordan Kober <153781917+jordankober@users.noreply.github.com>
2025-10-28 08:43:29 -0700

    initial go at LibrarianController

--- src/library/LibrarianController.java
+++ src/library/LibrarianController.java
@@ -7,5 +7,24 @@ public class LibrarianController {
 	// Delegates tasks to the appropriate objects.
 	
 	// TODO: implement functionality of Member class
+	private Library library;
+	
+	public LibrarianController(Library library) {
+		this.library = library; // gives controller access to book and member lists
+	}
+	
+	public void borrowBook(Member m, Book b) {
+		if(library.isAvailable(b)) { // check if book is available
+			m.borrowBook(); // delegate to member
+			b.manageState(); // change book state
+		}
+		else
+			System.out.println("Book unavailable");
+	}
+	
+	public void returnBook(Member m, Book b) {
+		m.returnBook();
+		b.manageState();
+	}
 
 }
